0d4ee7af20883be03310fe3b7693d1e2daad089f
佳佑 <bluesea@BlueSeadeMacBook-Pro.local>
2018-01-12 17:09:48 +0800

    Initial Commit

---
+++ AddressBookDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressBookDemo
+//
+//  Created by BlueSea on 2018/1/12.
+//  Copyright © 2018年 山茶花酿酒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddressBookDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AddressBookDemo
+//
+//  Created by BlueSea on 2018/1/12.
+//  Copyright © 2018年 山茶花酿酒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
